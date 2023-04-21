5c60f74044ddcac83ef6e21a00df1b86c0b725d2
Code Venom <saqibghaffar@outlook.com>
2023-04-21 17:30:48 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
+
+let multer = require('multer')
+
+app.post('/api/fileanalyse', multer().single('upfile'), (request, response) => {
+  let responseObject = {}
+  responseObject['name'] = request.file.originalname
+  responseObject['type'] = request.file.mimetype
+  responseObject['size'] = request.file.size
+  
+  response.json(responseObject)
+} )
